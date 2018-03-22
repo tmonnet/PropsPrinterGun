dc8f4e839734778a4eac74d9ef00027a6ad983f5
Unknown <t.monnet@rubika-edu.com>
2018-03-22 15:46:35 +0100

    Scan Done, Inventory Done, Printing WIP

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,163 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using DG.Tweening;
+
+public struct PropsSlot{
+	public float scanningDuration;
+	public float printingDuration;
+	public float printingCost;
+	public GameObject prefab;
+}
+
+public enum PlayerMode{
+	CombatMode,
+	BuildMode
+}
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
+	public Transform playerHead;
+	public float scanRange;
+	public float maxFireRate;
+	public float antimatterQuantity;
+	public float antimatterRegen;
+
+	[Header("UI")]
+	public Text[] inventoryText = new Text[4];
+	public Color selectedColor, baseColor;
+	public Slider antimatterSlider;
+
+	private PropsSlot[] inventory = new PropsSlot[4];
+
+	private PropsBehaviour _propsScanned = null;
+	private PropsBehaviour _propsTemp = null;
+	private RaycastHit _hit = new RaycastHit();
+	private PlayerMode _mode = PlayerMode.CombatMode;
+	private int _selectedSlot = 0;
+	private float _antimatterValue;
+
+	public void AddProps(GameObject prefab, float scanningDuration, float printingDuration, float printingCost){
+		if(prefab!=null){
+			inventory[_selectedSlot].prefab = prefab;
+			inventory[_selectedSlot].scanningDuration = scanningDuration;
+			inventory[_selectedSlot].printingDuration = printingDuration;
+			inventory[_selectedSlot].printingCost = printingCost;
+			inventoryText[_selectedSlot].DOText(prefab.name,0.5f);
+		}
+	}
+
 	void Start () {
-		
+		_antimatterValue = antimatterQuantity;
+		ChangeSlot(_selectedSlot);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+
+		// PRINTING  ------------------------------------------------------------
+
+		if(Input.GetButtonDown("Fire1")){
+			if(inventory[_selectedSlot].prefab != null){
+				if(_mode == PlayerMode.CombatMode){
+					//Instantiate and launch object
+					Instantiate(inventory[_selectedSlot].prefab, playerHead.position + playerHead.forward, playerHead.rotation);
+				}else{
+					//Instantiate object
+				}
+			}
+		}
+
+		// SCANNING  ------------------------------------------------------------
+
+		//Start scanning
+		if(Input.GetButtonDown("Fire2")){
+			StartScan();
+		}
+
+		//Stop scanning
+		if(Input.GetButton("Fire2")){
+			if(_propsScanned == null){
+				StartScan();
+			}else{
+				CheckScan();
+			}
+		}
+
+		//Stop scanning
+		if(Input.GetButtonUp("Fire2")){
+			StopScan();
+		}
+
+		// CHANGE SLOT -----------------------------------------------------------
+
+		if(Input.GetAxis("Mouse ScrollWheel") != 0f){
+			if(Input.GetAxis("Mouse ScrollWheel") > 0f){
+				if(_selectedSlot == 3){
+					ChangeSlot(0);
+				}else{
+					ChangeSlot(_selectedSlot + 1);
+				}
+			}else{
+				if(_selectedSlot == 0){
+					ChangeSlot(3);
+				}else{
+					ChangeSlot(_selectedSlot - 1);
+				}
+			}
+		}
+
+		if(Input.GetButtonDown("Hotkey1")){
+			ChangeSlot(0);
+		}
+
+		if(Input.GetButtonDown("Hotkey2")){
+			ChangeSlot(1);
+		}
+
+		if(Input.GetButtonDown("Hotkey3")){
+			ChangeSlot(2);
+		}
+
+		if(Input.GetButtonDown("Hotkey4")){
+			ChangeSlot(3);
+		}
+	}
+
+	private void StartScan(){
+		if(Physics.Raycast(playerHead.position, playerHead.forward, out _hit, scanRange)){
+			_propsScanned = _hit.collider.GetComponent<PropsBehaviour>();
+			if(_propsScanned != null){
+				_propsScanned.Scan(this);
+			}
+		}
+	}
+
+	private void CheckScan(){
+		if(Physics.Raycast(playerHead.position, playerHead.forward, out _hit, scanRange)){
+			Debug.Log("Check");
+			_propsTemp = _hit.collider.GetComponent<PropsBehaviour>();
+			if(_propsTemp != _propsScanned){
+				Debug.Log("Abort");
+				StopScan();
+			}
+		}else{
+			StopScan();
+		}
+	}
+
+	private void StopScan(){
+		Debug.Log("Stop");
+		if(_propsScanned != null){
+			_propsScanned.ScanStop();
+			_propsScanned = null;
+			_propsTemp = null;
+		}
+	}
+
+	private void ChangeSlot(int slot){
+		inventoryText[_selectedSlot].color = baseColor;
+		_selectedSlot = slot;
+		inventoryText[_selectedSlot].color = selectedColor;
 	}
 }
--- Assets/Scripts/PropsBehaviour.cs
+++ Assets/Scripts/PropsBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using DG.Tweening;
 
 public enum PropsState{
-
+	Preview_Idle,
+	Preview_Error,
+	Printed
 }
 
 public class PropsBehaviour : MonoBehaviour {
@@ -19,46 +21,58 @@ public class PropsBehaviour : MonoBehaviour {
 	private Material _scanMaterial;
 	private Collider _collider;
 	private Rigidbody _rb;
-	private bool _isProjectile = false;
 	private bool _isScanned = false;
 	private Tween _scanTween = null;
+	private PropsState _state;
 
 	void Awake()
 	{
 		_scanMaterial = GetComponent<Renderer>().materials[0];
 		_collider = GetComponent<Collider>();
 		_rb = GetComponent<Rigidbody>();
+		_state = PropsState.Printed;
 	}
 
 
 
 	//called when this props is printed
 	public void Print(){
-
+		_state = PropsState.Printed;
 	}
 
 	//called when the player start scanning this props
-	public void Scan(){ //add player as an argument
-		if(!_isScanned){
+	public void Scan(PlayerBehaviour player){ //add player as an argument
+		if(_state == PropsState.Printed && !_isScanned){
+			Debug.Log("Start Internal");
 			_isScanned = true;
-			_scanMaterial.DOFloat(1f, "_ScanValue", scanningDuration).OnComplete(
+			_scanMaterial.DOFloat(0.75f, "_ScanValue", scanningDuration).SetEase(Ease.Linear).OnComplete(
 				()=>{
-					_scanMaterial.SetFloat("_ScanValue", 0f);
+					_scanMaterial.SetFloat("_ScanValue", 0.25f);
 					_isScanned = false;
 					_scanTween = null;
+					player.AddProps(prefab, scanningDuration, printingDuration, printingCost);
+					//call method on the player to add this props in a slot
 				}
 			);
 
-			//call method on the player to add this props in a slot
 		}
 	}
 
 	//called when the player stop scanning this props
 	public void ScanStop(){
-		if(_scanTween != null && _isScanned){
+		if(_state == PropsState.Printed && _scanTween != null && _isScanned){
+			Debug.Log("Stop Internal");
 			_scanTween.Kill();
 			_scanTween = null;
+			_isScanned = false;
 			_scanMaterial.SetFloat("_ScanValue", 0f);
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(wallPenetration > 0f && other.transform.tag == "Penetrable"){
+
 		}
 	}
 }
